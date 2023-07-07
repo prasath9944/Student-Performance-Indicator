7a7332fc44db4c024855cc27eb828a209f336d73
Prasath <prasathks20@gmail.com>
2023-07-07 19:14:18 +0530

    changed model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -65,10 +65,6 @@ class ModelTrainer:
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
                 "CatBoosting Regressor":{
                     'depth': [6,8,10],
                     'learning_rate': [0.01, 0.05, 0.1],
