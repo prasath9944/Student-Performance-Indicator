b3a2a785fd5cc6afbabc239678b56abcfc76901d
Prasath <prasathks20@gmail.com>
2023-07-08 13:52:15 +0530

    Updated the application.py

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,render_template
+from flask_cors import CORS,cross_origin
 import numpy as np
 import pandas as pd
 
@@ -12,10 +13,12 @@ app=application
 ## Route for a home page
 
 @app.route('/')
+@cross_origin()
 def index():
     return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
+@cross_origin()
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
